a06fef35c89bff9a29d68596a00cb79c73c3029f
soumyaambewadi <soumyaambewadi@gmail.com>
2025-12-16 09:04:39 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,11 @@
 # palindrome_check.py
 # Program to check if a string is a palindrome
 
-import sys
+# Default string value
+string = "madam"
 
-# Check if Jenkins passes a command-line argument
-if len(sys.argv) > 1:
-    string = sys.argv[1]
-else:
-    # Fallback for manual/local runs
-    string = input("Enter a string: ")
-
-# Check palindrome
+# Converting to lowercase and checking
 if string.lower() == string[::-1].lower():
     print(f"'{string}' is a palindrome.")
 else:
     print(f"'{string}' is not a palindrome.")
-
